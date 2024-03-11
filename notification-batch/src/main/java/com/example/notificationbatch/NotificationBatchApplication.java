86955b371c5e938858e83ec1af44f433280d09d5
sangggyu <limsanggyu@kakao.com>
2024-03-11 14:56:26 +0900

    notification-server initial

---
+++ notification-batch/src/main/java/com/example/notificationbatch/NotificationBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificationbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationBatchApplication.class, args);
+	}
+
+}
---
+++ notification-batch/src/test/java/com/example/notificationbatch/NotificationBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notificationbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
