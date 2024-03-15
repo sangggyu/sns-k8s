0833f25c465c8c137d0b1c578aa26caa074ee62e
sangggyu <limsanggyu@kakao.com>
2024-03-15 15:54:21 +0900

    kafka set

--- user-server/src/main/java/com/example/userserver/follow/FollowService.java
+++ user-server/src/main/java/com/example/userserver/follow/FollowService.java
@@ -1,6 +1,9 @@
 package com.example.userserver.follow;
 
 import com.example.userserver.user.UserInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +14,12 @@ public class FollowService {
 
     private final FollowRepository followRepository;
 
-    public FollowService(FollowRepository followRepository) {
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public FollowService(FollowRepository followRepository, KafkaTemplate<String, String> kafkaTemplate) {
         this.followRepository = followRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public boolean isFollow(int userId, int followerId) {
@@ -25,6 +32,8 @@ public class FollowService {
             return null;
         }
 
+        sendFollowerMessage(userId, followerId, true);
+
         return followRepository.save(new Follow(userId, followerId));
     }
 
@@ -34,8 +43,20 @@ public class FollowService {
         if (follow == null) {
             return false;
         }
+
+        sendFollowerMessage(userId, followerId, false);
         followRepository.delete(follow);
         return true;
+    }
+
+    private void sendFollowerMessage(int userId, int followerId, boolean isFollow) {
+        FollowMessage message = new FollowMessage(userId, followerId, isFollow);
+
+        try {
+            kafkaTemplate.send("user.follower", objectMapper.writeValueAsString(message));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<UserInfo> listFollower(int userId) {
