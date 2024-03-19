6e99adb2f9c26daa8e0d1dc4ad22d51113b50b4d
sangggyu <limsanggyu@kakao.com>
2024-03-19 15:01:37 +0900

    timeline domain

--- timeline-server/src/main/java/com/example/timelineserver/timeline/LikeResponse.java
+++ timeline-server/src/main/java/com/example/timelineserver/timeline/LikeResponse.java
@@ -1,4 +1,19 @@
 package com.example.timelineserver.timeline;
 
 public class LikeResponse {
+    long likeCount;
+    boolean like;
+
+    public LikeResponse(long likeCount, boolean like) {
+        this.likeCount = likeCount;
+        this.like = like;
+    }
+
+    public long getLikeCount() {
+        return likeCount;
+    }
+
+    public boolean isLike() {
+        return like;
+    }
 }
